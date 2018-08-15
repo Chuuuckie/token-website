55a8ac60ec932785c71b09a2ac8258a94b8934b7
Sebastian Kim <sydneyitguy@gmail.com>
2018-08-15 18:49:33 +0900

    Use ruby 2.5.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.4.3'
+set :rbenv_ruby, '2.5.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
