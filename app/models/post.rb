30e031ab92971c9f1f5ec959f2c1fe1943e58170
Sebastian Kim <sydneyitguy@gmail.com>
2018-10-31 16:28:37 +0900

    Fix image tiles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   def thumbnail
-    "https://steemitimages.com/300x300/#{self.images[0]["link"]}"
+    image = if self.images[0]['link'] =~ /\.mp4$/
+      self.images[0]['link'].gsub(/\.mp4$/, '-thumb.jpg')
+    else
+      self.images[0]['link']
+    end
+    "https://steemitimages.com/300x300/#{image}"
   end
 end
