55accb3d9237f047e13ec59b8ce8d188411b52d7
Sebastian Kim <sydneyitguy@gmail.com>
2018-11-27 13:03:10 +0900

    Update influencers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,9 +12,9 @@ class HomeController < ApplicationController
     'geekgirl', 'playitforward', 'monajam', 'pialejoana'
   ].map {|person| { name: person, role: "Moderator", steemit: person} }
   INFLUENCER_ACCOUNTS = [
-    'elleok', 'chuuuckie', 'mobi72', 'fruitdaddy', 'karamyog', 'elsiekjay', 'calprut',
-    'ninuola', 'sonrhey', 'dayjee', 'camzy', 'abasifreke', 'gentleshaid', 'aamirijaz', 'tio',
-    'azwarrangkuti', 'ikrahch', 'rodus', 'sanach', 'alikoc07'
+    'camzy', 'ikrahch', 'aamirijaz', 'azwarrangkuti', 'abasifreke', 'sanach', 'rodus', 'gentleshaid', 'tio',
+    'xabi', 'giovaabbatichio', 'abdulmanan', 'sweetguy', 'mittymartz', 'wajahatsardar', 'faizan-ashraf',
+    'iqbaladan', 'yasayanoluler', 'shrazi', 'vinzie1'
   ].map {|person| { name: person, role: "Influencer", steemit: person} }
 
   THUMBNAIL_EXCLUSION = [
